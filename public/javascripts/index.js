2e93f34200a8f8d14c67199bc9ff8eb3581f6ace
Rafael J. Rodriguez <rafase282@gmail.com>
2016-03-06 05:17:06 +0000

    Add content to display the page

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,45 +1,51 @@
+var s = require("underscore.string");
 // FORMS CODE
 
 $('.form').find('input, textarea').on('keyup blur focus', function(e) {
 
-  var $this = $(this),
-    label = $this.prev('label');
+    var $this = $(this),
+        label = $this.prev('label');
 
-  if (e.type === 'keyup') {
-    if ($this.val() === '') {
-      label.removeClass('active highlight');
-    } else {
-      label.addClass('active highlight');
+    if (e.type === 'keyup') {
+        if ($this.val() === '') {
+            label.removeClass('active highlight');
+        }
+        else {
+            label.addClass('active highlight');
+        }
     }
-  } else if (e.type === 'blur') {
-    if ($this.val() === '') {
-      label.removeClass('active highlight');
-    } else {
-      label.removeClass('highlight');
+    else if (e.type === 'blur') {
+        if ($this.val() === '') {
+            label.removeClass('active highlight');
+        }
+        else {
+            label.removeClass('highlight');
+        }
     }
-  } else if (e.type === 'focus') {
+    else if (e.type === 'focus') {
 
-    if ($this.val() === '') {
-      label.removeClass('highlight');
-    } else if ($this.val() !== '') {
-      label.addClass('highlight');
+        if ($this.val() === '') {
+            label.removeClass('highlight');
+        }
+        else if ($this.val() !== '') {
+            label.addClass('highlight');
+        }
     }
-  }
 
 });
 
 $('.tab a').on('click', function(e) {
 
-  e.preventDefault();
+    e.preventDefault();
 
-  $(this).parent().addClass('active');
-  $(this).parent().siblings().removeClass('active');
+    $(this).parent().addClass('active');
+    $(this).parent().siblings().removeClass('active');
 
-  target = $(this).attr('href');
+    target = $(this).attr('href');
 
-  $('.tab-content > div').not(target).hide();
+    $('.tab-content > div').not(target).hide();
 
-  $(target).fadeIn(600);
+    $(target).fadeIn(600);
 
 });
 
@@ -51,186 +57,191 @@ var base_url = 'https://mangadb-r282.herokuapp.com';
 
 // Retrieves the token from local storage and returns it.
 function getToken() {
-  return window.localStorage.getItem("token");
+    return window.localStorage.getItem("token");
 }
 
 // Retrieves the token from local storage and returns it.
 function getPrevManga() {
-  return JSON.parse(window.localStorage.getItem("prevManga"));
+    return JSON.parse(window.localStorage.getItem("prevManga"));
 }
 
 // Deletes the token from local storage and removes manga from display.
 function logOut() {
-  window.localStorage.removeItem("token");
-  window.localStorage.removeItem("prevManga");
-  clean();
+    window.localStorage.removeItem("token");
+    window.localStorage.removeItem("prevManga");
+    clean();
 }
 
 // Removes manga from display
 function clean() {
-  $(".mangas").empty();
+    $(".mangas").empty();
 }
 
 // Retires the manga information and turns it into html and returns it
 function mangaInfo(manga) {
-  var title = '<h1>' + window.s.titleize(manga.title) + '</h1>';
-  var photo = '<img class="thumbnail" src="' + manga.thumbnail + '"</img>';
-  var author = '<span id="author"> <strong>Author:</strong> ' + window.s.titleize(manga.author) + '</span>';
-  var status = '<span id="status"> <strong>Status:</strong> ' + window.s.humanize(manga.seriesStatus) + '</span>';
-  var userStats = '<span id="userStats"> <strong>My Status:</strong> ' + window.s.humanize(manga.userStatus) + '</span>';
-  var chapter = '<span id="chapter"> <strong>Current Chapter:</strong> <a href="' + manga.url + '" target="_blank">' + manga.chapter + '</a></span><br>';
-  var type = '<span id="type"> <strong>Type:</strong> ' + window.s.humanize(manga.type) + '</span>';
-  var direction = '<span id="direction"> <strong>Reading Direction:</strong> ' + window.s.titleize(manga.direction) + '</span>';
-  var altName = '<span id="altName"> <strong>Other Names:</strong> ' + window.s.titleize(window.s.toSentence(manga.altName, ", ", ", ")) + '</span>';
-  var categories = '<span id="categories"> <strong>Categories:</strong> ' + window.s.titleize(window.s.toSentence(manga.categories, ", ", ", ")) + '</span>';
-  var plot = '<p id="plot"> <strong>Plot:</strong> ' + window.s.humanize(manga.plot) + '</p>';
-  var html = '<div class="manga-panel tg-wrap"><table><tr><th colspan="3">' + title + '</th></tr><tr><td rowspan="4">' + photo + '</td><td>' + status + '</td><td>' + userStats + '</td></tr><tr><td>' + author + '</td><td>' + direction + '</td></tr><tr><td>' + chapter + '</td><td>' + type + '</td></tr><tr><td>' + categories + '</td><td>' + altName + '</td></tr><tr><td colspan="3">' + plot + '</td></tr></table></div>';
-  return html;
-  
-  
+    var title = '<h1>' + s.titleize(manga.title) + '</h1>';
+    var photo = '<img class="thumbnail" src="' + manga.thumbnail + '"</img>';
+    var author = '<span id="author"> <strong>Author:</strong> ' + s.titleize(manga.author) + '</span>';
+    var status = '<span id="status"> <strong>Status:</strong> ' + s.humanize(manga.seriesStatus) + '</span>';
+    var userStats = '<span id="userStats"> <strong>My Status:</strong> ' + s.humanize(manga.userStatus) + '</span>';
+    var chapter = '<span id="chapter"> <strong>Current Chapter:</strong> <a href="' + manga.url + '" target="_blank">' + manga.chapter + '</a></span><br>';
+    var type = '<span id="type"> <strong>Type:</strong> ' + s.humanize(manga.type) + '</span>';
+    var direction = '<span id="direction"> <strong>Reading Direction:</strong> ' + s.titleize(manga.direction) + '</span>';
+    var altName = '<span id="altName"> <strong>Other Names:</strong> ' + s.titleize(s.toSentence(manga.altName, ", ", ", ")) + '</span>';
+    var categories = '<span id="categories"> <strong>Categories:</strong> ' + s.titleize(s.toSentence(manga.categories, ", ", ", ")) + '</span>';
+    var plot = '<p id="plot"> <strong>Plot:</strong> ' + s.humanize(manga.plot) + '</p>';
+    var html = '<div class="manga-panel tg-wrap"><table><tr><th colspan="3">' + title + '</th></tr><tr><td rowspan="4">' + photo + '</td><td>' + status + '</td><td>' + userStats + '</td></tr><tr><td>' + author + '</td><td>' + direction + '</td></tr><tr><td>' + chapter + '</td><td>' + type + '</td></tr><tr><td>' + categories + '</td><td>' + altName + '</td></tr><tr><td colspan="3">' + plot + '</td></tr></table></div>';
+    return html;
+
+
+
 }
 
 // FORM FUNCTIONS
 
 // Register new user.
 function regUser() {
-  var username = document.forms["signUp"]["username"].value;
-  var firstname = document.forms["signUp"]["firstname"].value;
-  var lastname = document.forms["signUp"]["lastname"].value;
-  var email = document.forms["signUp"]["email"].value;
-  var password = document.forms["signUp"]["password"].value;
-  var password2 = document.forms["signUp"]["password2"].value;
+    var username = document.forms["signUp"]["username"].value;
+    var firstname = document.forms["signUp"]["firstname"].value;
+    var lastname = document.forms["signUp"]["lastname"].value;
+    var email = document.forms["signUp"]["email"].value;
+    var password = document.forms["signUp"]["password"].value;
+    var password2 = document.forms["signUp"]["password2"].value;
 
-  var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": base_url + "/api/users",
-    "method": "POST",
-    "headers": {
-      "content-type": "application/x-www-form-urlencoded"
-    },
-    "data": {
-      "username": username,
-      "password": password,
-      "email": email,
-      "firstname": firstname,
-      "lastname": lastname
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": base_url + "/api/users",
+        "method": "POST",
+        "headers": {
+            "content-type": "application/x-www-form-urlencoded"
+        },
+        "data": {
+            "username": username,
+            "password": password,
+            "email": email,
+            "firstname": firstname,
+            "lastname": lastname
+        }
+    };
+
+    if (password === password2) {
+        $.ajax(settings).done(function(response) {
+            alert('User has been created');
+            console.log(response);
+        });
     }
-  };
-
-  if (password === password2) {
-    $.ajax(settings).done(function(response) {
-      console.log(response);
-    });
-  } else {
-    alert("No Matching password");
-  }
+    else {
+        alert("No Matching password");
+    }
 }
 
 // Logs in the user
 function logIn() {
-  var username = document.forms["login"]["username"].value;
-  var password = document.forms["login"]["password"].value;
-  var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": base_url + "/api/auth",
-    "method": "POST",
-    "headers": {
-      "content-type": "application/x-www-form-urlencoded"
-    },
-    "data": {
-      "username": username,
-      "password": password
-    }
+    var username = document.forms["login"]["username"].value;
+    var password = document.forms["login"]["password"].value;
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": base_url + "/api/auth",
+        "method": "POST",
+        "headers": {
+            "content-type": "application/x-www-form-urlencoded"
+        },
+        "data": {
+            "username": username,
+            "password": password
+        }
 
-  };
+    };
 
-  $.ajax(settings).done(function(data) {
-    window.localStorage.setItem('token', data.token);
-    console.log('Welcome back!');
-  });
+    $.ajax(settings).done(function(data) {
+        window.localStorage.setItem('token', data.token);
+        window.localStorage.setItem('MangaReader', username);
+        alert('Welcome back ' + username);
+        console.log('Welcome back!');
+    });
 }
 
 // Displays all mangas for current user.
 function getManga() {
-  var token = window.localStorage.getItem("token");
-  var username = document.forms["login"]["username"].value;
-  var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": base_url + "/api/mangas/" + username,
-    "method": "GET",
-    "headers": {
-      "x-access-token": token
-    }
-  };
+    var token = window.localStorage.getItem("token");
+    var username = window.localStorage.getItem("MangaReader");
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": base_url + "/api/mangas/" + username,
+        "method": "GET",
+        "headers": {
+            "x-access-token": token
+        }
+    };
 
-  $.ajax(settings).done(function(data) {
-    clean();
-    data.map(function(manga) {
-      var html = mangaInfo(manga);
-      $(".mangas").append(html);
+    $.ajax(settings).done(function(data) {
+        clean();
+        data.map(function(manga) {
+            var html = mangaInfo(manga);
+            $(".mangas").append(html);
+        });
     });
-  });
 }
 
 // Updates manga, currenlty increase manga chapter by one.
 function oneUp() {
-  var username = document.forms["login"]["username"].value;
-  var manga = encodeURI(document.getElementById('manga').value.toLowerCase());
-  var prevManga = getPrevManga();
-  var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": base_url + "/api/mangas/" + username + "/" + manga,
-    "method": "PUT",
-    "headers": {
-      "x-access-token": getToken(),
-      "content-type": "application/x-www-form-urlencoded"
-    },
-    "data": {
-      //"title": "Aiki",
-      //"author": "Isutoshi",
-      //"url": "http://www.readmanga.today/aiki",
-      //"userStatus": "finished",
-      //"type": "Japanese",
-      //"categories": "Action, Ecchi, Martial Arts, Mature, Seinen",
-      "chapter": prevManga.chapter + 1,
-      //"seriesStatus": "Completed",
-      //"plot": "There is fighting at the high school due to a power struggle for control. The granddaughter of the chief director requests help from the Aikido fighting style genius. Will he help? Or will he show his true colors with his bad boy ways?",
-      //"altName": "Love Air",
-      //"direction": "Right to Left",
-      //"thumbnail": "http://www.readmanga.today/uploads/posters/aiki.jpg"
-    }
-  };
+    var username = window.localStorage.getItem("MangaReader");
+    var manga = encodeURI(document.getElementById('manga').value.toLowerCase());
+    var prevManga = getPrevManga();
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": base_url + "/api/mangas/" + username + "/" + manga,
+        "method": "PUT",
+        "headers": {
+            "x-access-token": getToken(),
+            "content-type": "application/x-www-form-urlencoded"
+        },
+        "data": {
+            //"title": "Aiki",
+            //"author": "Isutoshi",
+            //"url": "http://www.readmanga.today/aiki",
+            //"userStatus": "finished",
+            //"type": "Japanese",
+            //"categories": "Action, Ecchi, Martial Arts, Mature, Seinen",
+            "chapter": prevManga.chapter + 1,
+            //"seriesStatus": "Completed",
+            //"plot": "There is fighting at the high school due to a power struggle for control. The granddaughter of the chief director requests help from the Aikido fighting style genius. Will he help? Or will he show his true colors with his bad boy ways?",
+            //"altName": "Love Air",
+            //"direction": "Right to Left",
+            //"thumbnail": "http://www.readmanga.today/uploads/posters/aiki.jpg"
+        }
+    };
 
-  $.ajax(settings).done(function(response) {
-    clean();
-    console.log(response);
-    window.localStorage.setItem('prevManga', manga);
-    findManga();    
-  });
+    $.ajax(settings).done(function(response) {
+        clean();
+        console.log(response);
+        window.localStorage.setItem('prevManga', manga);
+        findManga();
+    });
 }
 
 // Finds manga by title
 function findManga() {
-  var username = document.forms["login"]["username"].value;
-  var manga = encodeURI(document.getElementById('manga').value.toLowerCase());
-  var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": base_url + "/api/mangas/" + username + "/" + manga,
-    "method": "GET",
-    "headers": {
-      "x-access-token": getToken()
-    }
-  };
+    var username = window.localStorage.getItem("MangaReader");
+    var manga = encodeURI(document.getElementById('manga').value.toLowerCase());
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": base_url + "/api/mangas/" + username + "/" + manga,
+        "method": "GET",
+        "headers": {
+            "x-access-token": getToken()
+        }
+    };
 
-  $.ajax(settings).done(function(manga) {
-    window.localStorage.setItem('prevManga', JSON.stringify(manga));
-    clean();
-    var html = mangaInfo(manga);
-    $(".mangas").append(html);
-  });
+    $.ajax(settings).done(function(manga) {
+        window.localStorage.setItem('prevManga', JSON.stringify(manga));
+        clean();
+        var html = mangaInfo(manga);
+        $(".mangas").append(html);
+    });
 }
