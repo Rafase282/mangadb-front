efaf5f6206d6e307dd1b0fcc489f10d3f9748b0c
Rafael J. Rodriguez <rafase282@gmail.com>
2016-03-06 04:12:28 +0000

    Reset to express generator template to start over

--- app.js
+++ app.js
@@ -1,38 +1,60 @@
-// server.js
-'use strict';
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-// BASE SETUP
-// =============================================================================
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-// call the packages we need
-var express = require('express'); // call express
-var app = express(); // define our app using express
-require('dotenv').config({
-  silent: true
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-// configure the view
-app.use('/public', express.static(process.cwd() + '/public'));
+// error handlers
 
-// REGISTER OUR ROUTES -------------------------------
-var router = express.Router(); // get an instance of the express Router
-
-app.use('/', router);
-router.route('/')
-  .get(function(req, res) {
-    res.sendFile(process.cwd() + '/public/index.html');
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
   });
-  
-router.route('/signup')
-  .get(function(req, res) {
-      res.sendFile(process.cwd() + '/public/signup.html');
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
   });
-
-
-
-// CONFIGURE & START THE SERVER
-// =============================================================================
-var port = process.env.PORT || 8080; // set our port
-app.listen(port, function() {
-  console.log('Node.js listening on port ' + port);
 });
+
+
+module.exports = app;
