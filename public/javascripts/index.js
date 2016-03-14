0a32220bd1e111e1d068abdef2b8fff76deaad68
Rafael J. Rodriguez <rafase282@gmail.com>
2016-03-14 23:48:23 +0000

    View is progress

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -29,7 +29,7 @@ function mangaInfo(manga) {
 // FORM FUNCTIONS
 
 // Displays all mangas for current user.
-function getManga() {
+$(document).ready(function() {
     //var token = window.localStorage.getItem("token");
     //var username = window.localStorage.getItem("MangaReader");
     var settings = {
@@ -49,7 +49,7 @@ function getManga() {
             $(".mangas").append(html);
         });
     });
-}
+});
 
 // Updates manga, currenlty increase manga chapter by one.
 function oneUp() {
@@ -65,92 +65,13 @@ function oneUp() {
             "content-type": "application/x-www-form-urlencoded"
         },
         "data": {
-            //"title": "Aiki",
-            //"author": "Isutoshi",
-            //"url": "http://www.readmanga.today/aiki",
-            //"userStatus": "finished",
-            //"type": "Japanese",
-            //"categories": "Action, Ecchi, Martial Arts, Mature, Seinen",
-            "chapter": prevManga.chapter + 1,
-            //"seriesStatus": "Completed",
-            //"plot": "There is fighting at the high school due to a power struggle for control. The granddaughter of the chief director requests help from the Aikido fighting style genius. Will he help? Or will he show his true colors with his bad boy ways?",
-            //"altName": "Love Air",
-            //"direction": "Right to Left",
-            //"thumbnail": "http://www.readmanga.today/uploads/posters/aiki.jpg"
+            "chapter": prevManga.chapter + 1
         }
     };
 
     $.ajax(settings).done(function(response) {
         clean();
-        console.log(response);
         window.localStorage.setItem('prevManga', manga);
         findManga();
-    });
-}
-
-// Finds manga by title
-function findManga() {
-    var username = window.localStorage.getItem("MangaReader");
-    var manga = encodeURI(document.getElementById('manga').value.toLowerCase());
-    var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": api + "/mangas/" + user + "/" + manga,
-        "method": "GET",
-        "headers": {
-            "x-access-token": token
-        }
-    };
-
-    $.ajax(settings).done(function(manga) {
-        window.localStorage.setItem('prevManga', JSON.stringify(manga));
-        clean();
-        var html = mangaInfo(manga);
-        $(".mangas").append(html);
-    });
-}
-
-// Create new Mana
-
-function createManga() {
-    var title;
-    var author;
-    var url;
-    var userStatus;
-    var type;
-    var categories;
-    var chapter;
-    var seriesStatus;
-    var plot;
-    var altName;
-    var direction;
-    var thumbnail;
-    var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": api + "/mangas/" + user,
-        "method": "POST",
-        "headers": {
-            "x-access-token": token,
-            "content-type": "application/x-www-form-urlencoded"
-        },
-        "data": {
-            "title": "The God of High School",
-            "author": "PARK Yong Je",
-            "url": "http://www.readmanga.today/the-god-of-high-school",
-            "userStatus": "reading",
-            "type": "Japanese",
-            "categories": "Action, Adventure, Comedy, Martial Arts, Shounen",
-            "chapter": "249",
-            "seriesStatus": "Ongoing",
-            "plot": "While an island half-disappearing from the face of the earth, a mysterious organization is sending out invitations for a tournament to every skilled fighter in the world. “If you win you can have ANYTHING you want”They’re recruiting only the best to fight the best and claim the title of The God of high school!",
-            "altName": "God of Highschool, GoH",
-            "direction": "Right to Left",
-            "thumbnail": "http://www.readmanga.today/uploads/posters/the-god-of-high-school.jpg"
-        }
-    };
-
-    $.ajax(settings).done(function(response) {
-        console.log(response);
     });
 }
