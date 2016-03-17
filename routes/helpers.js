d2522eef30b29560349d292548083bc4c5dc1a5c
Rafael J. Rodriguez <rafase282@gmail.com>
2016-03-17 05:08:49 +0000

    Lint helpers file

--- routes/helpers.js
+++ routes/helpers.js
@@ -1,43 +1,10 @@
 /* This file handles additonal functions
  * that are needed across the app.
  */
-
 'use strict';
-var s = require("underscore.string");
-if (typeof localStorage === "undefined" || localStorage === null) {
-    var LocalStorage = require('node-localstorage').LocalStorage;
-    var localStorage = new LocalStorage('./storage');
-}
-
-// Retrieves the token from local storage and returns it.
-exports.getToken = function(name) {
-    return localStorage.getItem(name);
-};
-
-// Stores token to local storage
-exports.setToken = function(name, info) {
-    localStorage.setItem(name, info);
-};
-
-// Retrieves the token from local storage and returns it.
-exports.getObj = function(name) {
-    return JSON.parse(localStorage.getItem(name));
-};
-
-// Removes manga from display
-exports.clean = function() {
-    $(".mangas").empty();
-};
-
-// Deletes the token from local storage and removes manga from display.
-exports.logOut = function() {
-    localStorage.removeItem("token");
-    localStorage.removeItem("prevManga");
-    //clean();
-};
 
 // Checks to make sure used is logged in
-exports.isAuthenticated = function(req, res, next) {
+exports.isAuthenticated = function (req, res, next) {
     // Check to see if there is there is a user in session
     if (req.session.user !== undefined && req.session.user !== null) {
         return next();
@@ -45,25 +12,8 @@ exports.isAuthenticated = function(req, res, next) {
     res.redirect('/');
 };
 
-// Retires the manga information and turns it into html and returns it
-exports.mangaInfo = function(manga) {
-    var title = '<h1>' + s.titleize(manga.title) + '</h1>';
-    var photo = '<img class="thumbnail" src="' + manga.thumbnail + '"</img>';
-    var author = '<span id="author"> <strong>Author:</strong> ' + s.titleize(manga.author) + '</span>';
-    var status = '<span id="status"> <strong>Status:</strong> ' + s.humanize(manga.seriesStatus) + '</span>';
-    var userStats = '<span id="userStats"> <strong>My Status:</strong> ' + s.humanize(manga.userStatus) + '</span>';
-    var chapter = '<span id="chapter"> <strong>Current Chapter:</strong> <a href="' + manga.url + '" target="_blank">' + manga.chapter + '</a></span><br>';
-    var type = '<span id="type"> <strong>Type:</strong> ' + s.humanize(manga.type) + '</span>';
-    var direction = '<span id="direction"> <strong>Reading Direction:</strong> ' + s.titleize(manga.direction) + '</span>';
-    var altName = '<span id="altName"> <strong>Other Names:</strong> ' + s.titleize(s.toSentence(manga.altName, ", ", ", ")) + '</span>';
-    var categories = '<span id="categories"> <strong>Categories:</strong> ' + s.titleize(s.toSentence(manga.categories, ", ", ", ")) + '</span>';
-    var plot = '<p id="plot"> <strong>Plot:</strong> ' + s.humanize(manga.plot) + '</p>';
-    var html = '<div class="manga-panel tg-wrap"><table><tr><th colspan="3">' + title + '</th></tr><tr><td rowspan="4">' + photo + '</td><td>' + status + '</td><td>' + userStats + '</td></tr><tr><td>' + author + '</td><td>' + direction + '</td></tr><tr><td>' + chapter + '</td><td>' + type + '</td></tr><tr><td>' + categories + '</td><td>' + altName + '</td></tr><tr><td colspan="3">' + plot + '</td></tr></table></div>';
-    return html;
-};
-
 // Sets complete manga object
-exports.mangaObj = function(req) {
+exports.mangaObj = function (req) {
     return {
         title: req.body.title,
         author: req.body.author,
@@ -81,7 +31,7 @@ exports.mangaObj = function(req) {
 };
 
 // Sets complete manga object
-exports.userObj = function(req) {
+exports.userObj = function (req) {
     return {
         username: req.body.username,
         password: req.body.password,
@@ -92,7 +42,7 @@ exports.userObj = function(req) {
 };
 
 // Sets complete manga object
-exports.jadeObj = function(sess) {
+exports.jadeObj = function (sess) {
     return {
         title: sess.title,
         user: sess.user,
