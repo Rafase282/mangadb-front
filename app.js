67d8b75784effdb558af34cd71fd8032647e9590
Rafael J. Rodriguez <rafase282@gmail.com>
2016-03-25 15:55:50 +0000

    Switches to pug from jade

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(flash());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
