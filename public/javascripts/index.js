e747e6a6d8bc4b60da863977503db8c93c1ad12d
Rafael J. Rodriguez <rafase282@gmail.com>
2016-04-10 01:23:40 +0000

    trying to balance file length

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -138,3 +138,63 @@ $(document).ready(function() {
         getMangas();
     }
 });
+
+/* Increment chapter number
+ * Sends a PUT to the API with the new chapter number.
+ */
+function oneUp(info) {
+    var manga = info.split(','); // Split string into array
+    var mangaClass = '.' + manga[0]; // Select the class
+    var mangaTitle = manga[1];
+    var currentChapter = +$(mangaClass + ':first').text(); // Get value as int
+    var newChapter = currentChapter + 1; // increment chapter
+
+    var settings = {
+        'async': true,
+        'crossDomain': true,
+        'url': api + '/mangas/' + user.toLowerCase() + '/' +
+            encodeURIComponent(mangaTitle),
+        'method': 'PUT',
+        'headers': {
+            'x-access-token': token,
+            'content-type': 'application/x-www-form-urlencoded'
+        },
+        'data': {
+            'chapter': newChapter
+        }
+    };
+
+    $.ajax(settings).done(function() {
+        // Update chapter number in place
+        userMangas[mangaTitle].chapter = newChapter;
+        $(mangaClass).text(newChapter); // updates chapter for all catagories
+    });
+}
+
+/* Sends request to delete manga from DB
+ * and from the DOM itself
+ */
+
+function delManga(info) {
+    // Sends delete request and removes the manga from the DOM
+    var manga = info.split(','); // Split string into array
+    var mangaClass = '.' + manga[0]; // Select the class
+    var mangaTitle = manga[1];
+
+    var settings = {
+        'async': true,
+        'crossDomain': true,
+        'url': api + '/mangas/' + user.toLowerCase() + '/' +
+            encodeURIComponent(mangaTitle),
+        'method': 'DELETE',
+        'headers': {
+            'x-access-token': token,
+            'content-type': 'application/x-www-form-urlencoded'
+        }
+    };
+
+    // When the DEL request is done, delete from DOM
+    $.ajax(settings).done(function() {
+        $(mangaClass).remove();
+    });
+}
