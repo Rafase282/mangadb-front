55ca7ee49ba47e5d3fb378b0b454b1cf28597e2f
Rafael J. Rodriguez <rafase282@gmail.com>
2016-04-10 01:19:47 +0000

    Divide index.js into two parts

---
+++ public/javascripts/helper.js
@@ -0,0 +1,158 @@
+'use strict';
+/* Clear mangas from view
+ * Removes manga from all views and lists.
+ */
+function cleanMangas() {
+    $('.list').empty();
+    $('.list2').empty();
+    $('.list3').empty();
+    $('.list4').empty();
+}
+
+/* Clear User Info from view
+ * Removes user info from all views and lists.
+ */
+function cleanUser() {
+    $('.user-name').empty();
+    $('.user-email').empty();
+    $('.user-count').empty();
+}
+
+/* Manga object constructor
+ * Used to create a curated manga without saving
+ * sensitive information like _id and user id.
+ */
+function createManga(title, author, url, userStatus, type, categories, chapter,
+    seriesStatus, plot, altName, direction, thumbnail) {
+    this.title = title,
+        this.author = author,
+        this.url = url,
+        this.userStatus = userStatus,
+        this.type = type,
+        this.categories = categories,
+        this.chapter = chapter,
+        this.seriesStatus = seriesStatus,
+        this.plot = plot,
+        this.altName = altName,
+        this.direction = direction,
+        this.thumbnail = thumbnail;
+}
+
+/* Increment chapter number
+ * Sends a PUT to the API with the new chapter number.
+ */
+function oneUp(info) {
+    var manga = info.split(','); // Split string into array
+    var mangaClass = '.' + manga[0]; // Select the class
+    var mangaTitle = manga[1];
+    var currentChapter = +$(mangaClass + ':first').text(); // Get value as int
+    var newChapter = currentChapter + 1; // increment chapter
+
+    var settings = {
+        'async': true,
+        'crossDomain': true,
+        'url': api + '/mangas/' + user.toLowerCase() + '/' +
+            encodeURIComponent(mangaTitle),
+        'method': 'PUT',
+        'headers': {
+            'x-access-token': token,
+            'content-type': 'application/x-www-form-urlencoded'
+        },
+        'data': {
+            'chapter': newChapter
+        }
+    };
+
+    $.ajax(settings).done(function() {
+        // Update chapter number in place
+        userMangas[mangaTitle].chapter = newChapter;
+        $(mangaClass).text(newChapter); // updates chapter for all catagories
+    });
+}
+
+/* Sends request to delete manga from DB
+ * and from the DOM itself
+ */
+
+function delManga(info) {
+    // Sends delete request and removes the manga from the DOM
+    var manga = info.split(','); // Split string into array
+    var mangaClass = '.' + manga[0]; // Select the class
+    var mangaTitle = manga[1];
+
+    var settings = {
+        'async': true,
+        'crossDomain': true,
+        'url': api + '/mangas/' + user.toLowerCase() + '/' +
+            encodeURIComponent(mangaTitle),
+        'method': 'DELETE',
+        'headers': {
+            'x-access-token': token,
+            'content-type': 'application/x-www-form-urlencoded'
+        }
+    };
+
+    // When the DEL request is done, delete from DOM
+    $.ajax(settings).done(function() {
+        $(mangaClass).remove();
+    });
+}
+
+/* Search Bar Functionality
+ * Search for mangas on keypress.
+ * Search by author, title, alt name, and categories.
+ */
+
+function search() {
+    if ($('#search').val().length > 0) {
+        $('.manga-panel').css('display', 'none');
+        // Display the results
+        displayResults();
+    } else {
+        // display everything again
+        $('.manga-panel').css('display', 'block');
+    }
+
+    $('#search').unbind('keyup');
+    $('#search').keyup(search);
+}
+
+/* Display Search Results
+ * part of the search function.
+ */
+function displayResults() {
+    var reg = new RegExp($('#search').val(), 'ig');
+    for (var manga in userMangas) {
+        if (reg.test(userMangas[manga].title) ||
+            reg.test(userMangas[manga].altName) ||
+            reg.test(userMangas[manga].categories) ||
+            reg.test(userMangas[manga].author) ||
+            reg.test(userMangas[manga].type)) {
+            $('.panel-' + window.s.slugify(userMangas[manga].title))
+                .css('display', 'block');
+        }
+    }
+}
+/* Start Search
+ * When a key is released call the function.
+ */
+
+$('#search').keyup(search);
+
+/* Live Thubnail Preview
+ * When a key is release and the image input has text
+ * then use that text as the new src attribute for the image.
+ */
+function previewThumbnail() {
+    var img = $('#img-input').val();
+    if ($('#img-input').val().length > 4) {
+        $('#img').attr('src', img);
+    } else {
+        $('#img').attr('src', '../../images/kaneki1.jpg');
+    }
+
+    $('#img-input').unbind('keyup');
+    $('#img-input').keyup(previewThumbnail);
+}
+
+$('#img-input').keyup(previewThumbnail);
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -5,25 +5,6 @@ var user = user;
 var token = token;
 var userObj = {};
 
-/* Clear mangas from view
- * Removes manga from all views and lists.
- */
-function cleanMangas() {
-    $('.list').empty();
-    $('.list2').empty();
-    $('.list3').empty();
-    $('.list4').empty();
-}
-
-/* Clear User Info from view
- * Removes user info from all views and lists.
- */
-function cleanUser() {
-    $('.user-name').empty();
-    $('.user-email').empty();
-    $('.user-count').empty();
-}
-
 /* Manga panels generator
  * Takes a manga object and creates custom html
  * to be added to the dom.
@@ -67,26 +48,6 @@ function mangaInfo(manga) {
     var html = '<div class="col s12 m6 manga-panel ' + panelClass + '"><div class="card large" style="overflow: hidden;"><div class="card-image waves-effect waves-block waves-light blue darken-1 hoverable">' + photo + '</div><div class="card-content"> <span class="card-title activator grey-text text-darken-4">' + title + '<i class="material-icons right">more_vert</i></span> <br>' + chapter + direction + '</div><div class="card-action">' + buttons + '</div><div class="card-reveal" style="display: none; transform:' + ' translateY(0px);"><span class="card-title grey-text text-darken-4">' + title + '<br/><br/><i class="material-icons right">close</i></span> ' + altName + author + status + userStats + type + categories + plot + '</div></div></div>';
 
     return html;
-}
-
-/* Manga object constructor
- * Used to create a curated manga without saving
- * sensitive information like _id and user id.
- */
-function createManga(title, author, url, userStatus, type, categories, chapter,
-    seriesStatus, plot, altName, direction, thumbnail) {
-    this.title = title,
-        this.author = author,
-        this.url = url,
-        this.userStatus = userStatus,
-        this.type = type,
-        this.categories = categories,
-        this.chapter = chapter,
-        this.seriesStatus = seriesStatus,
-        this.plot = plot,
-        this.altName = altName,
-        this.direction = direction,
-        this.thumbnail = thumbnail;
 }
 
 /* Get list of all user's mangas
@@ -149,7 +110,7 @@ function getUserInfo() {
         userObj.firstname = window.s.titleize(userInfo.firstname);
         userObj.lastname = window.s.titleize(userInfo.lastname);
         userObj.email = userInfo.email;
-        userObj.count = Object.keys(userMangas).length
+        userObj.count = Object.keys(userMangas).length;
         var userName = '<span class="center-align"><h5 class="black-text">' + 'Full Name:</h5><h5> ' + userObj.firstname + ' ' + userObj.lastname + '</h5></span>';
         var userEmail = '<span class="center-align"><h5 class="black-text">E-Mail:</h5><h5> ' + userInfo.email + '</h5></span>';
         var userCount = '<span class="center-align"><h5 class="black-text">' + 'Total Manga Count:</h5><h5> ' + userObj.count + '</h5></span>';
@@ -165,7 +126,7 @@ function getUserInfo() {
  * call the function to get all mangas.
  */
 $(document).ready(function() {
-    $(".button-collapse").sideNav();
+    $('.button-collapse').sideNav();
     $('.modal-trigger').leanModal({
         dismissible: true, // Modal can be dismissed by clicking outside of it
         opacity: .5, // Opacity of modal background
@@ -177,122 +138,3 @@ $(document).ready(function() {
         getMangas();
     }
 });
-
-/* Increment chapter number
- * Sends a PUT to the API with the new chapter number.
- */
-function oneUp(info) {
-    var manga = info.split(','); // Split string into array
-    var mangaClass = '.' + manga[0]; // Select the class
-    var mangaTitle = manga[1];
-    var currentChapter = +$(mangaClass + ':first').text(); // Get value as int
-    var newChapter = currentChapter + 1; // increment chapter
-
-    var settings = {
-        'async': true,
-        'crossDomain': true,
-        'url': api + '/mangas/' + user.toLowerCase() + '/' +
-            encodeURIComponent(mangaTitle),
-        'method': 'PUT',
-        'headers': {
-            'x-access-token': token,
-            'content-type': 'application/x-www-form-urlencoded'
-        },
-        'data': {
-            'chapter': newChapter
-        }
-    };
-
-    $.ajax(settings).done(function() {
-        // Update chapter number in place
-        userMangas[mangaTitle].chapter = newChapter;
-        $(mangaClass).text(newChapter); // updates chapter for all catagories
-    });
-}
-
-/* Sends request to delete manga from DB
- * and from the DOM itself
- */
-
-function delManga(info) {
-    // Sends delete request and removes the manga from the DOM
-    var manga = info.split(','); // Split string into array
-    var mangaClass = '.' + manga[0]; // Select the class
-    var mangaTitle = manga[1];
-
-    var settings = {
-        'async': true,
-        'crossDomain': true,
-        'url': api + '/mangas/' + user.toLowerCase() + '/' +
-            encodeURIComponent(mangaTitle),
-        'method': 'DELETE',
-        'headers': {
-            'x-access-token': token,
-            'content-type': 'application/x-www-form-urlencoded'
-        }
-    };
-
-    // When the DEL request is done, delete from DOM
-    $.ajax(settings).done(function() {
-        $(mangaClass).remove();
-    });
-}
-
-/* Search Bar Functionality
- * Search for mangas on keypress.
- * Search by author, title, alt name, and categories.
- */
-
-function search() {
-    if ($('#search').val().length > 0) {
-        $('.manga-panel').css('display', 'none');
-        // Display the results
-        displayResults();
-    } else {
-        // display everything again
-        $('.manga-panel').css('display', 'block');
-    }
-
-    $('#search').unbind('keyup');
-    $('#search').keyup(search);
-}
-
-/* Display Search Results
- * part of the search function.
- */
-function displayResults() {
-    var reg = new RegExp($('#search').val(), 'ig');
-    for (var manga in userMangas) {
-        if (reg.test(userMangas[manga].title) ||
-            reg.test(userMangas[manga].altName) ||
-            reg.test(userMangas[manga].categories) ||
-            reg.test(userMangas[manga].author) ||
-            reg.test(userMangas[manga].type)) {
-            $('.panel-' + window.s.slugify(userMangas[manga].title))
-                .css('display', 'block');
-        }
-    }
-}
-/* Start Search
- * When a key is released call the function.
- */
-
-$('#search').keyup(search);
-
-/* Live Thubnail Preview
- * When a key is release and the image input has text
- * then use that text as the new src attribute for the image.
- */
-function previewThumbnail() {
-    var img = $('#img-input').val();
-    if ($('#img-input').val().length > 4) {
-        $('#img').attr('src', img);
-    } else {
-        $('#img').attr('src', '../../images/kaneki1.jpg');
-    }
-
-    $('#img-input').unbind('keyup');
-    $('#img-input').keyup(previewThumbnail);
-}
-
-$('#img-input').keyup(previewThumbnail);
