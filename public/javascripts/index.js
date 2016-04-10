126061ee367ecd58a5c823cbb3be2a9fee5ca732
Rafael J. Rodriguez <rafase282@gmail.com>
2016-04-10 02:41:30 +0000

    Shave lines off

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -5,9 +5,7 @@ var user = user;
 var token = token;
 var userObj = {};
 
-/* Clear mangas from view
- * Removes manga from all views and lists.
- */
+/* Clear mangas from view Removes manga from all views and lists. */
 function cleanMangas() {
     $('.list').empty();
     $('.list2').empty();
@@ -15,64 +13,39 @@ function cleanMangas() {
     $('.list4').empty();
 }
 
-/* Clear User Info from view
- * Removes user info from all views and lists.
- */
+/* Clear User Info from view Removes user info from all views and lists. */
 function cleanUser() {
     $('.user-name').empty();
     $('.user-email').empty();
     $('.user-count').empty();
 }
 
-/* Manga panels generator
- * Takes a manga object and creates custom html
- * to be added to the dom.
- */
+/* Manga panels generator: Takes a manga object and creates custom html to be added to the dom. */
 function mangaInfo(manga) {
     var inputClass = 'input-' + window.s.slugify(manga.title);
     var panelClass = 'panel-' + window.s.slugify(manga.title);
     var dataChapter = [inputClass, manga.title];
     var dataDel = [panelClass, manga.title];
-
     var title = window.s.titleize(manga.title);
-
     var photo = '<img class="activator thumbnail" src="' + manga.thumbnail + '"</img>';
-
     var author = '<span id="author"> <strong class="black-text">Author:</strong> ' + window.s.titleize(manga.author) + '</span><br/>';
-
     var status = '<span id="status"> <strong class="black-text">Series Status:</strong> ' + window.s.humanize(manga.seriesStatus) + '</span>&nbsp;&nbsp;';
-
     var userStats = '<span id="userStats"> <strong class="black-text">My Status:</strong> ' + window.s.humanize(manga.userStatus) + '</span>';
-
     var chapter = '<span class="float-right"> <strong class="black-text">Current Chapter: </strong>' + '<a class="' + inputClass + '" href="' + manga.url + '" target="_blank">' + manga.chapter + '</a></span>&nbsp;&nbsp;&nbsp;';
-
     var type = '<span id="type"> <strong class="black-text">Type:</strong> ' + window.s.humanize(manga.type) + '</span><br>';
-
     var direction = '<span id="direction"><strong class="black-text">Reading Direction:</strong>' + ' ' + window.s.titleize(manga.direction) + '</span><br/>';
-
     var altName = '<span id="altName"> <strong class="black-text">Other Names:</strong> ' + window.s.titleize(window.s.toSentence(manga.altName, ', ', ', ')) + '</span><br/><br/>';
-
     var categories = '<span id="categories"> <strong class="black-text">Categories:</strong> ' + window.s.titleize(window.s.toSentence(manga.categories, ', ', ', ')) + '</span>';
-
     var plot = '<p id="plot"> <strong class="black-text">Plot:</strong> ' + window.s.humanize(manga.plot) + '</p><br/><br/>';
-
     var addOne = '<a class="btn tooltipped blue darken-1" data-position="bottom" data-delay="50" data-tooltip="Increase Chapter Number by One" onclick="oneUp(\'' + dataChapter + '\')">' + '<i class="material-icons">plus_one</i></a>';
-
     var del = '<a class="btn tooltipped blue darken-1" data-position="bottom" data-delay="50" data-tooltip="Delete Manga" onclick="delManga(\'' + dataDel + '\')"><i class="material-icons">delete</i></a>';
-
     var upd = '<a class="btn tooltipped blue darken-1" data-position="bottom" data-delay="50" data-tooltip="Update Manga Information" href=\'/user/' + user.toLowerCase() + '/' + encodeURIComponent(manga.title) + '\'"><i class="material-icons">update</i></a>';
-
     var buttons = '<div class="row center-align">' + '<div class="input-field.col.s12.m6">' + del + addOne + upd + '</div></div>';
-
     var html = '<div class="col s12 m6 manga-panel ' + panelClass + '"><div class="card large" style="overflow: hidden;"><div class="card-image waves-effect waves-block waves-light blue darken-1 hoverable">' + photo + '</div><div class="card-content"> <span class="card-title activator grey-text text-darken-4">' + title + '<i class="material-icons right">more_vert</i></span> <br>' + chapter + direction + '</div><div class="card-action">' + buttons + '</div><div class="card-reveal" style="display: none; transform:' + ' translateY(0px);"><span class="card-title grey-text text-darken-4">' + title + '<br/><br/><i class="material-icons right">close</i></span> ' + altName + author + status + userStats + type + categories + plot + '</div></div></div>';
-
     return html;
 }
 
-/* Manga object constructor
- * Used to create a curated manga without saving
- * sensitive information like _id and user id.
- */
+/* Manga object constructor: Used to create a curated manga without saving sensitive information like _id and user id. */
 function createManga(title, author, url, userStatus, type, categories, chapter,
     seriesStatus, plot, altName, direction, thumbnail) {
     this.title = title,
@@ -89,12 +62,7 @@ function createManga(title, author, url, userStatus, type, categories, chapter,
         this.thumbnail = thumbnail;
 }
 
-/* Get list of all user's mangas
- * Sends a GET request to the API and generates an oject
- * directory with curated manga information, sensitive data
- * is not kept.
- */
-
+/* Get list of all user's mangas: Sends a GET request to the API and generates an oject directory with curated manga information, sensitive data is not kept. */
 function getMangas() {
     var settings = {
         'async': true,
@@ -105,7 +73,6 @@ function getMangas() {
             'x-access-token': token
         }
     };
-
     $.ajax(settings).done(function(mangas) {
         cleanMangas();
         mangas.data.map(function(manga) {
@@ -113,10 +80,8 @@ function getMangas() {
                 manga.userStatus, manga.type, manga.categories, manga.chapter,
                 manga.seriesStatus, manga.plot, manga.altName, manga.direction,
                 manga.thumbnail);
-
             userMangas[manga.title] = newManga;
             var html = mangaInfo(newManga);
-
             if (newManga.userStatus === 'reading') {
                 $('.list2').append(html);
             } else if (newManga.userStatus === 'finished') {
@@ -143,7 +108,6 @@ function getUserInfo() {
             'x-access-token': token
         }
     };
-
     $.ajax(settings).done(function(response) {
         var userInfo = response.data[0];
         userObj.firstname = window.s.titleize(userInfo.firstname);
@@ -157,13 +121,9 @@ function getUserInfo() {
         $('.user-name').append(userName);
         $('.user-email').append(userEmail);
         $('.user-count').append(userCount);
-
     });
 }
-/* Load Manga info When Page Is Loaded
- * When the profile page is read it will
- * call the function to get all mangas.
- */
+/* Load Manga info When Page Is Loaded: When the profile page is read it will call the function to get all mangas. */
 $(document).ready(function() {
     $('.button-collapse').sideNav();
     $('.modal-trigger').leanModal({
@@ -178,16 +138,13 @@ $(document).ready(function() {
     }
 });
 
-/* Increment chapter number
- * Sends a PUT to the API with the new chapter number.
- */
+/* Increment chapter number: Sends a PUT to the API with the new chapter number. */
 function oneUp(info) {
     var manga = info.split(','); // Split string into array
     var mangaClass = '.' + manga[0]; // Select the class
     var mangaTitle = manga[1];
     var currentChapter = +$(mangaClass + ':first').text(); // Get value as int
     var newChapter = currentChapter + 1; // increment chapter
-
     var settings = {
         'async': true,
         'crossDomain': true,
@@ -202,24 +159,17 @@ function oneUp(info) {
             'chapter': newChapter
         }
     };
-
     $.ajax(settings).done(function() {
-        // Update chapter number in place
         userMangas[mangaTitle].chapter = newChapter;
         $(mangaClass).text(newChapter); // updates chapter for all catagories
     });
 }
 
-/* Sends request to delete manga from DB
- * and from the DOM itself
- */
-
+/* Sends request to delete manga from DB and from the DOM itself */
 function delManga(info) {
-    // Sends delete request and removes the manga from the DOM
     var manga = info.split(','); // Split string into array
     var mangaClass = '.' + manga[0]; // Select the class
     var mangaTitle = manga[1];
-
     var settings = {
         'async': true,
         'crossDomain': true,
@@ -231,34 +181,24 @@ function delManga(info) {
             'content-type': 'application/x-www-form-urlencoded'
         }
     };
-
-    // When the DEL request is done, delete from DOM
     $.ajax(settings).done(function() {
         $(mangaClass).remove();
     });
 }
 
-/* Search Bar Functionality
- * Search for mangas on keypress.
- * Search by author, title, alt name, and categories.
- */
-
+/* Search Bar Functionality: Search for mangas on keypress. Search by author, title, alt name, and categories. */
 function search() {
     if ($('#search').val().length > 0) {
         $('.manga-panel').css('display', 'none');
-        // Display the results
         displayResults();
     } else {
-        // display everything again
         $('.manga-panel').css('display', 'block');
     }
-
     $('#search').unbind('keyup');
     $('#search').keyup(search);
 }
 
-/* Display Search Results
- * part of the search function.
+/* Display Search Results, part of the search function.
  */
 function displayResults() {
     var reg = new RegExp($('#search').val(), 'ig');
@@ -273,16 +213,10 @@ function displayResults() {
         }
     }
 }
-/* Start Search
- * When a key is released call the function.
- */
-
+/* Start Search when a key is released call the function. */
 $('#search').keyup(search);
 
-/* Live Thubnail Preview
- * When a key is release and the image input has text
- * then use that text as the new src attribute for the image.
- */
+/* Live Thubnail Preview: When a key is release and the image input has text then use that text as the new src attribute for the image. */
 function previewThumbnail() {
     var img = $('#img-input').val();
     if ($('#img-input').val().length > 4) {
@@ -290,7 +224,6 @@ function previewThumbnail() {
     } else {
         $('#img').attr('src', '../../images/kaneki1.jpg');
     }
-
     $('#img-input').unbind('keyup');
     $('#img-input').keyup(previewThumbnail);
 }
